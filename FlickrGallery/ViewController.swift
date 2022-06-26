07c9d63f0a820d205ce8939523f78e76f934e367
Yuliia Khrupina <julichka1391@gmail.com>
2022-06-25 20:09:58 -0700

    added clear button

--- FlickrGallery/ViewController.swift
+++ FlickrGallery/ViewController.swift
@@ -7,14 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
     
     @IBOutlet weak var textView: UITextField!
+    @IBOutlet weak var clearTextFieldButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    func eraseSearch() {}
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
     }
+    
+    // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {}
+    
+    func willDisplay() {}
+    
+    func searchBarButtonTapped() {}
+    
+    func textSearchAction(text: String) {}
 }
 
