924ab38f62036ef1271ac1c617d8f3a01b36f969
Yuliia Khrupina <julichka1391@gmail.com>
2022-06-25 19:58:55 -0700

    added textfield + collection view

--- FlickrGallery/ViewController.swift
+++ FlickrGallery/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextField!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
