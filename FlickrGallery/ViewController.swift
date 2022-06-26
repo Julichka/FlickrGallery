85f4eb3dd54eb42ae286a636af45f16e7b6e3fea
Yuliia Khrupina <julichka1391@gmail.com>
2022-06-25 21:05:34 -0700

    photos call

--- FlickrGallery/ViewController.swift
+++ FlickrGallery/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
@@ -14,10 +15,37 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var clearTextFieldButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     
+    let FLICKR_SEARCH_URL = "https://www.flickr.com/services/api/explore/flickr.photos.search"
+    let API_KEY = "ff83247590ff7fe2a1967cc9add636e9"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        
+        requestTest(searchWord: "cat")
+    }
+    
+    func requestTest(searchWord: String) {
+        let requestURL = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(API_KEY)&text=\(searchWord)&format=json&nojsoncallback=1"
+        
+        AF.request(requestURL, method: .get).responseJSON { (responce) in
+            switch responce.result {
+            case .success(let value):
+                guard let castingValue = value as? [String: Any] else { return }
+                print(castingValue)
+               // guard let userData = Mapper<NewsListRestObjects>().map(JSON: castingValue) else { return }
+               // self.articles = (userData.articles)!
+               // print(self.articles.count)
+               // self.table.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func getImageUrl(serverId: String, id: String, secret: String, size: String) -> String {
+        return "https://live.staticflickr.com/\(serverId)/\(id)_\(secret)_\(size).jpg"
     }
     
     func eraseSearch() {}
