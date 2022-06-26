866265f2a9d26b787a47a8a1eec4a698a63331b8
Yuliia Khrupina <julichka1391@gmail.com>
2022-06-25 19:45:00 -0700

    Initial Commit

---
+++ FlickrGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrGallery
+//
+//  Created by Yuliia Khrupina on 6/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
