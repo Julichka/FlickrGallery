6fe8768e808e4ee6b7b574040e1eefe68ae399d4
Yuliia Khrupina <julichka1391@gmail.com>
2022-06-25 19:52:23 -0700

    added navigation controller

---
+++ FlickrGallery/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  FlickrGallery
+//
+//  Created by Yuliia Khrupina on 6/25/22.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
